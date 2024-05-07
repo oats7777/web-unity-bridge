8befc74c102fcd7ef97352e19da1cc081bbc119a
oats7777 <hanmin84200@gmail.com>
2024-05-07 14:45:12 +0900

    hotfix - release 1.0.5

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,6 @@
 import { BridgeCore } from "./methods/bridgeCore";
+import type { UnityInstance } from "./declarations/unity-instance";
 
-export type { UnityInstance } from "./declarations/unity-instance";
+export type { UnityInstance };
+
 export default BridgeCore;
